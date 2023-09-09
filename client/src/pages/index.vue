d7f06ed6ebe02b6988222231592a3adc00c96263
tadaki <tadaki.asechi@gmail.com>
2023-09-09 16:16:32 +0900

    separate mobile code for another vue file

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,15 +1,25 @@
 <template>
     <div>
-        <CommandPrompt />
+        <CommandPromptMobile v-if="isMobile" />
+        <CommandPrompt v-else />
     </div>
 </template>
 
 <script>
-import CommandPrompt from "~/components/CommandPrompt.vue";
+import CommandPrompt from '~/components/CommandPrompt.vue';
+import CommandPromptMobile  from '~/components/CommandPromptMobile.vue';
+
+let isMobile = false;
+
+if (process.client) { // Ensure this runs only on client-side
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    isMobile = /mobile|iphone|ipad|android/.test(userAgent);
+}
 
 export default {
     components: {
-        CommandPrompt
+        CommandPrompt,
+        CommandPromptMobile,
     }
 };
 </script>
