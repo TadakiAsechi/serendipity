28962802bfb5c04121e86829f77bc87307bb3d6b
tadaki <tadaki.asechi@gmail.com>
2023-09-13 22:24:28 +0900

    robot 追加

--- client/src/pages/secondPage.vue
+++ client/src/pages/secondPage.vue
@@ -1,5 +1,41 @@
 <template>
-    <div>
-        <h1>開発中</h1>
+    <div class="container">
+        <img class="centered-image" src="~/assets/img/robo.png" />
+        <div class="dialogue-box">
+            カイハツチュウ
+        </div>
     </div>
 </template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+.container {
+    height: 100vh;
+    background-color: black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start; /* Start aligning from the top */
+    padding-top: 5%; /* Adjust this value to move the image higher or lower */
+}
+
+
+.centered-image {
+    max-width: 50%;
+    max-height: 50%;
+}
+
+.dialogue-box {
+    width: 80%;
+    height: 30%;
+    border: thick solid;
+    border-radius: 10px;
+    border-color: aliceblue;
+    color: lime;
+    margin-top: 20px;
+    padding: 10px 20px;
+    overflow: auto;
+}
+</style>
