df162c3292c2e7061aa5224f0fca2729b73b330a
tadaki <tadaki.asechi@gmail.com>
2023-09-14 13:18:27 +0900

    secondPage mobile対応の準備

--- client/src/pages/secondPage.vue
+++ client/src/pages/secondPage.vue
@@ -1,66 +1,34 @@
 <template>
-    <div class="container">
-        <img class="centered-image" src="~/assets/img/robo.png" />
-        <div class="dialogue-box">
-            カイハツチュウ
-        </div>
+    <div>
+        <desktop />
     </div>
 </template>
 
-<script setup lang="ts">
+<script>
+import { ref,onMounted } from "vue";
+
+import desktop from '~/components/secondPage/desktop.vue';
+// import mobile  from '~/components/secondPage/mobile.vue';
+// import matrixRain  from '~/components/matrixRain/matrixRain.vue';
+
+
+export default {
+    setup() {
+        const isMobile = ref(false);
+
+        onMounted(() => {
+            const userAgent = window.navigator.userAgent.toLowerCase();
+            console.log(userAgent)
+            isMobile.value = /mobile|iphone|ipad|android/.test(userAgent);
+        })
+
+        return { isMobile };
+
+    },
+    components: {
+        desktop,
+        // mobile,
+        // matrixRain,
+    }
+};
 </script>
-
-<style>
-* {
-    box-sizing: border-box;
-}
-
-.container {
-    height: 100vh;
-    background-color: black;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-start;
-    padding-top: 5%;
-}
-
-
-.centered-image {
-    max-width: 50%;
-    max-height: 50%;
-    opacity: 0; 
-    animation: fadeIn 5s forwards;
-}
-
-.dialogue-box {
-    width: 80%;
-    max-height: 260px;
-    height: 40%; 
-    border: thick solid;
-    border-radius: 10px;
-    border-color: aliceblue;
-    color: lime;
-    margin-top: 20px;
-    padding: 10px 20px;
-    overflow: auto;
-    opacity: 0; 
-    animation: fadeIn 5s forwards;
-}
-
-@media (max-width: 735px) {
-    .dialogue-box {
-        max-height: 400px;
-    }
-    .centered-image {
-        min-height: 320px;
-        min-width: 320px;
-    }
-}
-
-@keyframes fadeIn {
-    to {
-        opacity: 1;
-    }
-}
-</style>
